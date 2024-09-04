27e0b1821f05a251fa3ea7aa27aaa36b4cc4375f
leep16 <leep16@mcmaster.ca>
2024-09-04 18:10:50 -0400

    Added Draggable. TODO: make icons snap to grid

--- src/sections/MainScreen.jsx
+++ src/sections/MainScreen.jsx
@@ -1,12 +1,24 @@
 import React, { useEffect, useRef, useState } from 'react';
 import PDF from '../icons/pdf.svg';
 import Grid from '@mui/material/Grid2';
+import Draggable from 'react-draggable';
 
 const MainScreen = () => {
   const ref = useRef(null);
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   const [icons, setIcons] = useState([PDF]);
+
+  const [deltaPosition, setDeltaPosition] = useState([0, 0]);
+
+  const handleDrag = (e, ui) => {
+    setDeltaPosition([e.clientX, e.clientY]);
+  };
+
+  const handleStop = () => {
+    console.log("stopped");
+    console.log(deltaPosition);
+  }
 
   useEffect(() => {
     // when the component gets mounted
@@ -22,10 +34,11 @@ const MainScreen = () => {
     return () => window.removeEventListener("resize", getsize);
   }, [ref]);
 
+  // TODO: maybe remove the array. With the Draggable, I can make it so I can just calculate the locations
+  // of the grid, and make it snap to the nearest slot. No array needed
   useEffect(() => {
-    console.log(ref.current.offsetWidth);
     if (icons.length === 1 && width > 0 && height > 0) {
-      var a = new Array(Math.round((width / 118.47619) * (height / 127.2)) - 2).fill(null);
+      var a = new Array(Math.round(Math.floor((width + 40) / 120) * Math.floor((height - 40) / 120)) - icons.length).fill(null);
       setIcons((icons) => [...icons, ...a]);
     }
   }, [height, width]);
@@ -35,10 +48,12 @@ const MainScreen = () => {
         <Grid container spacing={5} className='w-full' column={16}>
             {icons.map((c) => {
             return (
+              <Draggable onDrag={handleDrag} onStop={handleStop}>
               <Grid item>
-                {c !== null ? <div className='items-center justify-center align-middle flex-col w-20 h-20 text-white text-center'><img src={PDF} key={c} className='w-16 h-16' /><div className='w-16 h-16'>My Resume</div></div> : <div className='w-20 h-20'></div>}
-              </Grid>)
-            })}
+                {c !== null ? <div className='items-center justify-center align-middle flex-col w-20 h-20 text-white text-center'><img src={PDF} key={c} className='w-16 h-16 pointer-events-none' /><div className='w-16 h-16'>My Resume</div></div> : <div className='w-20 h-20'></div>}
+              </Grid>
+              </Draggable>
+              )})}
             {/* <button onClick={() => {setIcons((icons) => [null, ...icons])}}>Press</button> */}
         </Grid>
       </section>
