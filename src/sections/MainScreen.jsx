f39bc15344e3fc12aaf204bd4890977af9eedb52
leep16 <leep16@mcmaster.ca>
2024-09-06 19:13:26 -0400

    ADDED SNAPPING!!! WOOOOO

--- src/sections/MainScreen.jsx
+++ src/sections/MainScreen.jsx
@@ -8,6 +8,8 @@ const MainScreen = () => {
   const [width, setWidth] = useState(0);
   const [height, setHeight] = useState(0);
   const [icons, setIcons] = useState([PDF]);
+  const [hover, setHover] = useState(-1);
+  const [positions, setPositions] = useState([{ x: 0, y: 0}]);
 
   const [deltaPosition, setDeltaPosition] = useState([0, 0]);
 
@@ -15,9 +17,50 @@ const MainScreen = () => {
     setDeltaPosition([e.clientX, e.clientY]);
   };
 
-  const handleStop = () => {
-    console.log("stopped");
-    console.log(deltaPosition);
+  const handleClick = (e) => {
+    
+    e = e || window.e;
+    var target = e.srcElement;
+    console.log([e.clientX, e.clientY]);
+    var rect = target.getBoundingClientRect();
+    console.log(rect.top, rect.left, rect.bottom, rect.right);
+
+    // console.log(isInAnIcon(e.clientX, e.clientY));
+  }
+
+  const isInAnIcon = (x,y) => {
+    if (x < 0 || x > width || y < 0 || y > height) {
+      return false;
+    }
+    
+    let ax = x - 8;
+    let ay = y - 8;
+
+    let inSquareX = (ax % 120) < 80;
+    let inSquareY = (ay % 120) < 80;
+
+    return inSquareX && inSquareY;
+  }
+
+  const snapToGrid = (x,y) => {
+    const sx = Math.floor((x - 8) / 120) * 120;
+    const sy = Math.floor((y - 8) / 120) * 120;
+
+    return { x: sx, y: sy };
+  }
+
+  const handleStop = (e, ui, index) => {
+    if (isInAnIcon(e.clientX, e.clientY)) {
+      const newPosition = snapToGrid(e.clientX, e.clientY);
+      const updatedPositions = [...positions];
+      updatedPositions[index] = newPosition;
+      setPositions(updatedPositions);
+    }
+  }
+
+  const handleHover = (index) => {
+    console.log("hovered over " + index);
+    setHover(index);
   }
 
   useEffect(() => {
@@ -30,30 +73,33 @@ const MainScreen = () => {
       setHeight(ref.current.offsetHeight - 16);
     };
     window.addEventListener("resize", getsize);
+    // window.addEventListener("click", handleClick);
     // remove the event listener before the component gets unmounted
     return () => window.removeEventListener("resize", getsize);
   }, [ref]);
 
   // TODO: maybe remove the array. With the Draggable, I can make it so I can just calculate the locations
   // of the grid, and make it snap to the nearest slot. No array needed
-  useEffect(() => {
-    if (icons.length === 1 && width > 0 && height > 0) {
-      var a = new Array(Math.round(Math.floor((width + 40) / 120) * Math.floor((height - 40) / 120)) - icons.length).fill(null);
-      setIcons((icons) => [...icons, ...a]);
-    }
-  }, [height, width]);
+  // useEffect(() => {
+  //   if (icons.length === 1 && width > 0 && height > 0) {
+  //     var a = new Array(Math.round(Math.floor((width + 40) / 120) * Math.floor((height - 40) / 120)) - icons.length).fill(null);
+  //     setIcons((icons) => [...icons, ...a]);
+  //   }
+  // }, [height, width]);
 
   return (
       <section ref={ref} className='w-full h-full absolute p-2' style={{ flexGrow: 1 }}>
         <Grid container spacing={5} className='w-full' column={16}>
-            {icons.map((c) => {
-            return (
-              <Draggable onDrag={handleDrag} onStop={handleStop}>
-              <Grid item>
-                {c !== null ? <div className='items-center justify-center align-middle flex-col w-20 h-20 text-white text-center'><img src={PDF} key={c} className='w-16 h-16 pointer-events-none' /><div className='w-16 h-16'>My Resume</div></div> : <div className='w-20 h-20'></div>}
-              </Grid>
-              </Draggable>
-              )})}
+            {icons.map((c, index) => {
+                return (
+                    <Draggable position={positions[index]} onDrag={handleDrag} onStop={(e, ui) => handleStop(e, ui, index)} >
+                      <Grid item>
+                        <div className='items-center justify-center align-middle flex-col w-20 h-20 text-white text-center'><img src={PDF} key={c} className='w-16 h-16 pointer-events-none block m-auto' /><div className='w-16 h-16 block m-auto'>My Resume</div></div>
+                      </Grid>
+                  </Draggable>
+                  )
+              }
+              )}
             {/* <button onClick={() => {setIcons((icons) => [null, ...icons])}}>Press</button> */}
         </Grid>
       </section>
