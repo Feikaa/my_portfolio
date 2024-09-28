83561bba72ddc40fd626a926ddbc9e8d7dcd4a0a
Paul <kafei42@gmail.com>
2024-09-28 10:39:52 -0400

    updated about me

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 function About() {
   return (
     <div className='text-white p-2 font-light'>
-        I am a Computer Science from McMaster University who graduated on June 2023. I'm passionate about web development, with hands-on experience in React, JavaScript, TypeScript, and NodeJS. I'm eager to begin my career and am open to roles in front-end, 
-        back-end, or full-stack development. I am constantly looking to challenge myself by learning new skills, and expanding my knowledge in web development. I am eager to show my current skills and knowledge to any work/projects I do. Creating sleek, 
-        user-friendly websites with highly functional, efficient, and robust backends is what drives my passion and motivation as a web developer. In my free time, I enjoy playing video games, learning to speak new languages, and baking.
+        Hi! I'm Paul. I grew up in Sarnia, Ontario. In school, I gravitated towards math, and spent a lot of my free time on the computer. I quickly learned how much I adored computers, and learned everything I could about them. In high school, I tried programming for the first
+        time, along with basic HTML, which is when I seriously considered becoming a programmer, so I applied to McMaster University for a Computer Science major in 2018. During my first year in university, I took a programming class, and the satisfaction and joy I felt from creating my first program was when I knew I picked the right career path. From then
+        on, my passion and love for creating things through programming has stayed strong. I studied Computer Science for 5 years and graduated in June 2023. I'm currently based in Ontario, Canada. I'm passionate about web development, with hands-on experience in React, JavaScript, TypeScript, and NodeJS. 
+        I'm eager to begin my career and am open to roles in front-end, 
+        back-end, or full-stack development. I am constantly looking to challenge myself by learning new skills, expanding my knowledge in web development, and applying my current skills and knowledge to any work/projects I do. Creating sleek, 
+        user-friendly websites with highly functional, efficient, and robust backends, along with being able to build and see projects I participate in slowly come to life drives my passion and motivation as a web developer.
+        <br />
+        In my free time, I enjoy playing video games, learning to speak new languages such as Korean and Japanese, and baking sweets!
     </div>
   )
 }
